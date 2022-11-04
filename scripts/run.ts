ae27678837cb597abd6ee142bffc9049d7d5ca6f
Tugkan Kibar <tugkan@orangebandits.com>
2022-11-04 23:14:10 +0100

    Add run script

---
+++ scripts/run.ts
@@ -0,0 +1,20 @@
+import hre from "hardhat";
+
+async function main() {
+  const domainContractFactory = await hre.ethers.getContractFactory("Domains");
+  const domainContract = await domainContractFactory.deploy();
+  await domainContract.deployed();
+  console.log("Contract deployed to: ", domainContract.address);
+}
+
+async function runMain() {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+runMain();
