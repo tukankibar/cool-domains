38ef66a8d984e0d4733e3b33246fdcb0290a48cd
Tugkan Kibar <tugkan@orangebandits.com>
2022-11-05 02:38:25 +0100

    Initial registration logic

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -2,10 +2,55 @@
 
 pragma solidity ^0.8.17;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
-  constructor() {
-    console.log("Domains contract.");
+  string public tld;
+
+  mapping(string => address) public domains;
+  mapping(string => string) public records;
+
+  constructor(string memory _tld) payable {
+    tld = _tld;
+    console.log("%s name service deployed.", _tld);
+  }
+
+  function price(string calldata name) public pure returns (uint) {
+    uint len = StringUtils.strlen(name);
+
+    require(len > 0);
+
+    if (len == 3) {
+      return 5 * 10**17;
+    } else if (len == 4) {
+      return 3 * 10**17;
+    } else {
+      return 1 * 10**17;
+    }
+  }
+
+  function register(string calldata name) public payable {
+    require(domains[name] == address(0)); // Name is unregistered
+
+    uint _price = price(name);
+
+    require(msg.value >= _price, "Not enough $MATIC paid.");
+
+    domains[name] = msg.sender;
+    console.log("%s has registered a domain!", msg.sender);
+  }
+
+  function getAddress(string calldata name) public view returns (address) {
+    return domains[name];
+  }
+
+  function setRecord(string calldata name, string calldata record) public {
+    require(domains[name] == msg.sender); // Owner is the tx sender
+    records[name] = record;
+  }
+
+  function getRecord(string calldata name) public view returns (string memory) {
+    return records[name];
   }
 }
--- scripts/run.ts
+++ scripts/run.ts
@@ -1,10 +1,33 @@
 import hre from "hardhat";
 
 async function main() {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
-  const domainContract = await domainContractFactory.deploy();
+  const domainContract = await domainContractFactory.deploy("social");
   await domainContract.deployed();
+
   console.log("Contract deployed to: ", domainContract.address);
+  console.log("Contract deployed by: ", owner.address);
+
+  let txn = await domainContract.register("doom", {
+    value: hre.ethers.utils.parseEther("1"),
+  });
+  await txn.wait();
+
+  const domainOwner = await domainContract.getAddress("doom");
+  console.log("Owner of domain: ", domainOwner);
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance: ", hre.ethers.utils.formatEther(balance));
+
+  try {
+    txn = await domainContract
+      .connect(randomPerson)
+      .setRecord("doom", "Haha mine!");
+    await txn.wait();
+  } catch (error) {
+    console.log("Tx failed successfully.");
+  }
 }
 
 async function runMain() {
