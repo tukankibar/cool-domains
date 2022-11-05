0a35c336a90dd3e40f4c4904833cdc9db6e0888b
Tugkan Kibar <tugkan@orangebandits.com>
2022-11-05 21:59:17 +0100

    Add testnet deployment config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+function transformPrivateKeyToAccount(key: string | undefined): string {
+  if (key === undefined) {
+    throw new Error("Account private key is not provided.");
+  }
+
+  return `0x${key}`;
+}
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "testnet",
+  networks: {
+    testnet: {
+      url: process.env.TESTNET_URL,
+      accounts: [transformPrivateKeyToAccount(process.env.PRIVATE_KEY)],
+    },
+  },
 };
 
 export default config;
