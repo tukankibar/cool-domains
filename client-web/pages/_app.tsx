296e088b598cea187d38a1099ce4d878461bf807
Tugkan Kibar <tugkan@orangebandits.com>
2022-11-06 11:11:32 +0100

    Add layout to app level

--- client-web/pages/_app.tsx
+++ client-web/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import { Footer } from "components/footer";
+import { Header } from "components/header";
+
+import styles from "styles/App.module.css";
+
+import "styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.container}>
+      <Header />
+      <main className={styles.main}>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>
+  );
 }
